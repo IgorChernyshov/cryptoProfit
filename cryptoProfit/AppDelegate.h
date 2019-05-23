63b8da47d684ab932256e823f5b85b37e8e9f23e
Igor Chernyshov <ichernyshov@bk.ru>
2019-05-23 17:05:37 +0300

    Initial Commit

---
+++ cryptoProfit/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  cryptoProfit
+//
+//  Created by Igor Chernyshov on 23/05/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ cryptoProfit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  cryptoProfit
+//
+//  Created by Igor Chernyshov on 23/05/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
