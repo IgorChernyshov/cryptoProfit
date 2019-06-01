c955ddb81de9c35b7ff3242c89975faba04062b4
Igor Chernyshov <ichernyshov@bk.ru>
2019-06-01 14:33:36 +0300

    Added custom color palette

---
+++ cryptoProfit/UIColor+CPTColors.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+CPTColors.h
+//  cryptoProfit
+//
+//  Created by Igor Chernyshov on 01/06/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (CPTColors)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cryptoProfit/UIColor+CPTColors.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+CPTColors.m
+//  cryptoProfit
+//
+//  Created by Igor Chernyshov on 01/06/2019.
+//  Copyright © 2019 Igor Chernyshov. All rights reserved.
+//
+
+#import "UIColor+CPTColors.h"
+
+@implementation UIColor (CPTColors)
+
+@end
